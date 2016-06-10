9a45dfd0d0a3a0570c38dc260ea03731daa01980
Michael <michael.loukeris@icloud.com>
2016-06-10 15:02:09 +0300

    Authenticate and Login User

--- testBuildings/accounts/views.py
+++ testBuildings/accounts/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 from rest_framework.response import Response
 from rest_framework.generics import GenericAPIView
 
 
 class AccountLoginView(GenericAPIView):
     def post(self, request, *args, **kwargs):
-        print request.data
-        return Response("ok", status=200)
+        print request.data['username']
+        user = authenticate(username=request.data['username'], password=request.data['password'])
+        if user.is_active:
+            login(request, user)
+            return Response('Logged In', status=200)
+        else:
+            return Response("ok", status=200)
